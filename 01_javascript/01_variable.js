e7de74cccdf38f2847d9a329763d426c7d99926b
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-08-05 10:39:29 +0000

    Variable concept

---
+++ 01_javascript/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail ="pritamrauniyar@google.com"
+var accountPassword = "123456"
+accountCity = "kathmandu"
+
+//accountId = 2 // NOT ALLOWED 
+accountEmail ="jklajfdl.com"
+accountPassword = "12244999"
+accountCity = "Birgunj"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+/*
+prefer not use var
+because  of issue in block scope and functional
+*/
