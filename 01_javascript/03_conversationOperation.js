7ca41394bcc9d9edbd2e45968c99ff549736be78
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-08-08 09:20:24 +0000

    Coversation  of the datatype concept

---
+++ 01_javascript/03_conversationOperation.js
@@ -0,0 +1,25 @@
+//let score = "100abc"
+// let score = true
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumver = Number(score)
+// console.log(typeof valueInNumver)
+// console.log(valueInNumver)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+let isLoggedIn = 0
+let  booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log( booleanIsLoggedIn);
+// 1 => true; 0 => false
+// "" => false
+// "Pritam Rauniyar" => true
+
+
+let myNum = 578
+
+let stringNum = String(myNum)
+console.log(stringNum);
+console.log(typeof stringNum);
