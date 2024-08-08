a1dfeed22377f25bf62037dbd6d8f9214c227b99
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-08-08 08:42:25 +0000

    Datatype concept

---
+++ 01_javascript/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer version
+// alert(3 + 3) // we are using nodejs  we can't run it in nodejs we aren't using browser
+// console.log(3 +
+// 3)// it's not good programming in programming we should to maintaine the readablity
+// console.log("Pritam Rauniyar")
+
+let name = "Pritam Rauniyar"
+let age = 20
+let isLoggedIn = false
+
+
+
+// Primative Datatype
+// number => 2 to power 53
+// bidint
+// string => ""
+// boolean => true/false
+// null => stadalone value
+// undefined => those variable which value is not assing
+// symbol => unique
+
+
+//object => bundle of the array or data those will be many more
