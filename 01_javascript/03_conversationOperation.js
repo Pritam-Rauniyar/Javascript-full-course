9c86d277c965e6d65dbf26c062fe1e89aeb0b39d
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-08-09 09:08:06 +0000

    we know about conversation and operator

--- 01_javascript/03_conversationOperation.js
+++ 01_javascript/03_conversationOperation.js
@@ -10,16 +10,63 @@
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
-let isLoggedIn = 0
-let  booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log( booleanIsLoggedIn);
+// let isLoggedIn = 0
+// let  booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log( booleanIsLoggedIn);
 // 1 => true; 0 => false
 // "" => false
 // "Pritam Rauniyar" => true
 
 
-let myNum = 578
+// let myNum = 578
 
-let stringNum = String(myNum)
-console.log(stringNum);
-console.log(typeof stringNum);
+// let stringNum = String(myNum)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+
+// ####################### Operation %%%%%%%%%%%%%%%%
+// let value = 3 
+// let negValue =  -value
+// console.log(negValue)
+// some practicle operations
+// console.log(2 + 2); // it's use to add any variable or value
+// console.log(2 -2 ); // it's use to substract the value or variable
+// console.log(2 * 2); // it's use to multiple any value
+// console.log(2 ** 2 ); // It's use to square the value
+// console.log(4 / 2); // It's use to divide the value 
+// console.log( 3 % 2 ); // It's use to find out the remainder of the value
+
+// let str1 = "hello"
+// let str2 = " Pritam Rauniyar"
+// let str3 = str1 + str2
+// console.log(str3 + "!")
+
+
+// Some more to know
+// console.log( "1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 4) * (5 % 3));
+
+// console.log(+true); it's not good practice of code coding in IT field
+
+// let num1 , num2, num3 
+// num1 = num2 = num3 = 2 + 2 (It's not good coding practice in IT field or to readablity)
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+// Note- to know more about operetors and coversations visti the ECMA script link is here https://tc39.es/ecma262/#sec-type-conversion
