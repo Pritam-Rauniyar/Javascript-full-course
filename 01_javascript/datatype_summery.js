95c67b63d8c6027a54ec2c0e9d387c2b2fe47cba
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-08-10 12:13:23 +0000

    summery of the datatype

---
+++ 01_javascript/datatype_summery.js
@@ -0,0 +1,34 @@
+// Primitive datatyype
+// there are 7 type of primitavie datatype
+//string
+// let userName = "Pritam Rauniyar" 
+// number
+// const Marks= 96
+// boolean
+// const IsLoggedIn = true
+// null
+// const outSideTemp = null
+// undefined
+// let name;
+// symbol
+//const Id = Symbol ('578')
+//const anoytherId = Symbol ('578')
+// bigit
+// const bigNumber = p9767550927
+
+// non-Primitive datatype(Refrence)
+// Array
+//const mountains = ["Himal"," Sagarmatha" , " Lhotse"]
+// Objects
+// const student ={
+//     name : "Pritam",
+//     age : 23,
+//     phone: 9767550927
+// }
+// Function
+// const myFunction = function(){
+//     console.log("Hello Pritam Rauniyar");
+// }
+
+// to findout the type of the data use typeof (put the value)
+// console.log( typeof "Pritam Rauniyar")
