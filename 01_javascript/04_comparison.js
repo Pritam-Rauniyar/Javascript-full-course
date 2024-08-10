52c670e98d80ab26a8657907f9f483807ad21450
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-08-10 11:48:04 +0000

    It's about the comparison of value concept

---
+++ 01_javascript/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log ( 2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log( "2" > 1);
+// console.log( "02" > 1);
+
+// console.log ( null > 0);
+// console.log ( null == 0);
+// console.log ( null >= 0);
+// note:- javascript convert the value null is 0 
+// console.log ( undefined > 0);
+// console.log ( undefined == 0);
+// console.log ( undefined >= 0);
+//note :- javascript convert the value which is in the assing value is 1 
+
+// === It's check the value strictly
