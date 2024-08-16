256633d07814ae58fd792f2fcfebf9b7da68bc85
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-08-16 12:45:21 +0000

    string updated with deep knowledge and to know more visit to the MDN documentation

---
+++ 01_javascript/05_string.js
@@ -0,0 +1,37 @@
+// const Name = "Pritam Rauniyar"
+// const Phone = 9817280578  
+// console.log(Name +" Contact "+ Phone); // it's not good way to code
+// console.log(`Hello there My Name is ${Name.toUpperCase()} My contact whatsapp and contact number is ${Phone}`); //it's the good way and modern way to code
+
+
+// const gameName = new String("Pritam-RAuniyar")
+// console.log(gameName.split('-'));
+
+// console.log(gameName[6]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('R'));
+// const newSring = gameName.substring(0, 4)
+// console.log(newSring);
+// const anotherSting = gameName.slice(-15, 5)
+// console.log(anotherSting);
+//  const newString =  "        Pritam Rauniyar"
+//  console.log(newString);
+//  console.log(newString.trim());
+
+//  const url = "https://pritamrauniyar.com/pritam%home"
+//  console.log(url.replace('%', '-'));
+ 
+ // checkout the value is in cluded or not 
+//  console.log(url.includes('pritam'));
+ 
+ 
+ 
+
+
+
+
+
+
+
