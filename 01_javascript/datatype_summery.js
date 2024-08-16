8161cc979b54f5f73234f826c22e54f0ed391b0b
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-08-16 11:54:58 +0000

    Stack and Heap update

--- 01_javascript/datatype_summery.js
+++ 01_javascript/datatype_summery.js
@@ -32,3 +32,30 @@
 
 // to findout the type of the data use typeof (put the value)
 // console.log( typeof "Pritam Rauniyar")
+
+// ****************************************
+// stack (Primitive), Heap (Non-Primitive/Refrence(object))
+ 
+// let myName = "Pritam Rauniyar"
+// let anotherName = myName
+// anotherName = "Pritam Gupta"
+
+// console.log( myName);
+// console.log(anotherName);
+
+// let user1 = {
+//     email: "Pritamsahrauniyar@gmail.com",
+//     phone: 9817280578,
+//     FacebooK: "Pritam Rauniyar"
+// }
+
+// let user2 = user1
+// user2.email = "Pritamsahrauniyar@google.com"
+
+// // console.log(user1.email);
+// console.log(user2.email);
+
+
+// Note:- in stack if we make any changes that time we get a copy where in raw data nothing will be changed
+// Note2:- In Heap if we make any changes that time we get refrence of the data and raw data will be changed in original data
+
