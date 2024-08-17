ac6218cd537dead5a6c95a2ff05a892976937cd2
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-08-17 01:04:45 +0000

    Math_and_Number

---
+++ 01_javascript/06_math_and_num.js
@@ -0,0 +1,36 @@
+// const marks = 350
+// console.log(marks);
+
+// const balance = new Number(200.00223)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 45.6879
+// console.log(otherNumber.toPrecision(3));
+
+
+// const hundreds = 1000000000
+// console.log(hundreds.toLocaleString('en-NP'));
+
+
+//_____________________________Math=================
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(8.9));
+// console.log(Math.ceil(5.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4 , 5, 7, 9, 11, 22));
+// console.log(Math.max(1 , 3, 9, 8, 80, 25));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log((Math.random()*10) + 1);
+
+
+const min = 12
+const max = 50
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
