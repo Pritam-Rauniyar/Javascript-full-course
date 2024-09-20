9eddc6f44c4a3cbcbee4351f60eab8bda51449cc
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-09-20 08:01:46 +0000

    Date & Time updated with full explaination and examples

--- 01_javascript/07_dates.js
+++ 01_javascript/07_dates.js
@@ -1,1 +1,24 @@
+ // Dates 
+//  let myDate = new Date()
+//  console.log(myDate);
+//  console.log(myDate.toString());
+//  console.log(myDate.toLocaleString());
+//  console.log(typeof myDate);
+
+//  let myCreatedDate = new Date( 2081, 5, 3)
+//  let myCreatedDate = new Date( 2025, 8, 19, 4, 2)
+// let myCreatedDate = new Date("09-19-2024") 
+//  console.log(myCreatedDate.toLocaleString());
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+let newDate = new Date()
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+
+
+
+
  
