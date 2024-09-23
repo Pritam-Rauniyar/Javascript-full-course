1575ecea0ea9e594b75d10d2ac2d9b67d6225e94
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-09-23 06:28:49 +0000

    all tips and knowledge is updated

--- 01_javascript/07_dates.js
+++ 01_javascript/07_dates.js
@@ -1,10 +1,9 @@
- // Dates 
+// Dates 
 //  let myDate = new Date()
 //  console.log(myDate);
 //  console.log(myDate.toString());
 //  console.log(myDate.toLocaleString());
-//  console.log(typeof myDate);
-
+//  console.log(typeof myDate);  
 //  let myCreatedDate = new Date( 2081, 5, 3)
 //  let myCreatedDate = new Date( 2025, 8, 19, 4, 2)
 // let myCreatedDate = new Date("09-19-2024") 
@@ -13,12 +12,6 @@
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
 // console.log(Math.floor(Date.now()/1000));
-let newDate = new Date()
+// let newDate = new Date()
 // console.log(newDate.getMonth()+1);
 // console.log(newDate.getDay());
-
-
-
-
-
- 
