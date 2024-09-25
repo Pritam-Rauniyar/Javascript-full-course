1e60eed8b1bcb315e59b033cdb2a8fd1e2999dc4
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-09-25 11:23:44 +0000

    Finally Arrays updated

--- 02_javascript/01_arrays.js
+++ 02_javascript/01_arrays.js
@@ -1,5 +1,5 @@
-// Arrays
-const Hero = ["Amitab Bachan", "Shahrukh Khan", "Amir Khana"]
-const myHero = ["Shusant singh", "Sunny Deol", "Abhisek Bachan"]
-const myHero1 = Hero.concat(myHero)
-console.log(Hero);
+// array
+let myArr =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+let myHeroes = ["Amitabh Bachan", "Shahrukh Khan" , "Amir Khan"]
+let myHeroes1 = myArr.concat(myHeroes)
+console.log(myHeroes1)
