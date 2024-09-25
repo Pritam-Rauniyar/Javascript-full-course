e587486db803f8f11c04b02eecad0b913726ae1c
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-09-25 08:28:21 +0000

    arrays updated

---
+++ 02_javascript/01_arrays.js
@@ -0,0 +1,5 @@
+// Arrays
+const Hero = ["Amitab Bachan", "Shahrukh Khan", "Amir Khana"]
+const myHero = ["Shusant singh", "Sunny Deol", "Abhisek Bachan"]
+const myHero1 = Hero.concat(myHero)
+console.log(Hero);
