5279f2456d3a91792017af417da079d7b016ba56
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-09-26 16:35:22 +0000

    New updated

--- 02_javascript/01_arrays.js
+++ 02_javascript/01_arrays.js
@@ -1,5 +1,38 @@
 // array
-let myArr =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-let myHeroes = ["Amitabh Bachan", "Shahrukh Khan" , "Amir Khan"]
-let myHeroes1 = myArr.concat(myHeroes)
-console.log(myHeroes1)
+// let myArr =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// let myHeroes = ["Amitabh Bachan", "Shahrukh Khan" , "Amir Khan"]
+// let myHeroes1 = myArr.concat(myHeroes)
+// console.log(myArr)
+//arrays method
+// myArr.push(14)
+// myArr.push(15)
+// myArr.pop()
+// myArr.unshift(16)
+// myArr.shift()
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(5))
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr)
+// console.log(typeof newArr)
+
+// slice, splice
+// console.log("A", myArr);
+// const myn1 = myArr.slice(1, 3)
+// console.log(myn1);
+// console.log("B", myArr);
+// const myn2 = myArr.splice(1, 3)
+// console.log("C", myArr);
+// console.log(myn2);
+const MyHeroes = ["Hitesh", "Raghav" , "Remo"]
+const MySinger = ["Pritam", "Jubin Nutiyal", "Rahat Fatehi"]
+
+// MyHeroes.push(MySinger)
+// console.log(MyHeroes)
+// const allhero = MyHeroes.concat(MySinger)
+// console.log(allhero);
+//  const allHero = [...MyHeroes, ...MySinger]
+//  console.log(allHero);
+// const MyArr = [1, 2, 3, [4, 5, 6], 7, [8, 9, 10]]
+// const myArr2 = MyArr.flat(Infinity)
+// console.log(myArr2);
