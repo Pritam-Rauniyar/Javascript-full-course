119279da8e8dcb3be29991b9d47f047100e1f289
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-09-30 14:08:06 +0000

    IIFE completed

---
+++ 03_javascript/04_IIFE.js
@@ -0,0 +1,14 @@
+// immediately Invoked function  Expressions (IIFE)
+// (function Pritam() {
+//     console.log(`DB connected`);    
+// })()
+// IIFe is used to ignore the global Pollution sample of this is below
+// (function name(){
+
+// })()
+// ( () => {
+//  console.log(`DB connected`);
+// })();
+// ( (name) => {
+//  console.log(`DB connected ${name}`);
+// })("pritam");
