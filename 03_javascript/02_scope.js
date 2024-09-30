fbd58117693f79dbd2a39d02040cfcd4834a780d
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-09-30 11:18:33 +0000

    scope knowledge

---
+++ 03_javascript/02_scope.js
@@ -0,0 +1,11 @@
+// global scope
+if (true) { // sample of the block scope 
+    let a = 10
+    const b = 20 
+    var c = 30 // var has global scope it's mean that we can use the value of the var from outside of scope and in the scope
+}
+// console.log(a);
+// console.log(b);
+// console.log(c); // i preffer to ignore the use of the var
+
+
