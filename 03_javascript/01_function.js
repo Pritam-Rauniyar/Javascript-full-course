de55ff5be66723d7a74c6e65af38a7b339f0d389
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-09-30 10:21:15 +0000

    function basic completed

---
+++ 03_javascript/01_function.js
@@ -0,0 +1,65 @@
+// function a interesting chapter of the coding by using the function we can make any work easy than we do individually
+// console.log("P")
+// console.log("R")
+// console.log("I")
+// console.log("T")
+// console.log("A")
+// console.log("M")
+// we can use here function to do it simple and sweet with the help of function let's go
+// function MyName(){
+//     console.log("P")
+//     console.log("R")
+//     console.log("I")
+//     console.log("T")
+//     console.log("A")
+//     console.log("M")
+// }
+// MyName()
+// without brackets we can't excute the code 
+// Important thing is that if we don't declare any name of the function than we can excute the code in short we are going to face the error in excutation of the code
+
+// another example of the function
+// function addMyNumber(val1, val2)//this call parameters
+// {
+//     console.log(val1 + val2);
+// }
+// function addMyNumber(val1, val2)//this call parameters
+// {
+//     return val1 + val2 // another method to write the same code
+//     // let Result = val1 + val2
+//     // return Result
+    
+// }
+// let Result = addMyNumber(10, 25)// this call arguments
+// console.log("Result =", Result);
+
+// function FullName(MyName){
+//     if (MyName === "") {
+//         return `please enter you name`
+//     }
+//     return`good to see you with us in javascript learning with ${MyName} `
+// }
+// console.log(FullName( "Pritam Rauniyar"));
+// // seprate the different value in one array by using ...(three dots are use to seprate the value in one array)
+// function cartPrice(...value){
+//     return value
+
+// }
+// console.log(cartPrice(200, 500, 600, 700));
+ 
+// // object function 
+// const userName = {
+//     username: "Pritam Rauniyar",
+//     price: 199
+// }
+// function handleObject (myObj){
+//     console.log(`Username is ${myObj.username} and price is ${myObj.price}`);
+    
+// }
+// handleObject(userName)
+// const myArr = [2, 3, 4, 5, 6, 7,]
+// function handleArr(Array){
+//     return Array[2]
+// }
+// // console.log(handleArr(myArr))
+// console.log(handleArr([2, 3, 6, 5]))
