17657046c560fc849d9a5988e075ceab766da5a7
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-09-30 12:00:13 +0000

    scope updated

--- 03_javascript/02_scope.js
+++ 03_javascript/02_scope.js
@@ -1,11 +1,40 @@
 // global scope
-if (true) { // sample of the block scope 
-    let a = 10
-    const b = 20 
-    var c = 30 // var has global scope it's mean that we can use the value of the var from outside of scope and in the scope
-}
+// if (true) { // sample of the block scope 
+//     let a = 10
+//     const b = 20 
+//     var c = 30 // var has global scope it's mean that we can use the value of the var from outside of scope and in the scope
+// }
 // console.log(a);
 // console.log(b);
 // console.log(c); // i preffer to ignore the use of the var
+// function Bar(){
+//     const UserName = "Pritam Rauniyar"
+//     function Menu(){
+//         const gmail = "Pritamrauniyar@gmail.com"
+//         console.log(UserName);
+        
+//     }
+//     // console.log(gmail)
+//     // Menu()
+// }
+// // Bar()
+// if (true) {
+//     const MyName = "Pritam Rauniyar"
+//     if (MyName === "Pritam Rauniyar") {
+//         const gmail = "Pritamrauniyar@gmail.com"
+//         console.log(MyName + " " + gmail);
+//     }
+    // console.log(gmail); // we can't get access
+    
 
+// console.log(MyName); // we can't get access
+//    ++++++++++++++++++ some more concept +++++++++++++++++++++++++++++
+// console.log(addone(7))
+//    function addone(value){
+//     return value + 1
+//    }
 
+//    const addtwo = function(value1){
+//        return value1 + 2
+//     }
+// console.log(addtwo(9));
