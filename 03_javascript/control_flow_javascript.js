0991c0ab1a2cc5ef44338b21601505c3fb421cec
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-10-01 12:12:37 +0000

    control flow and conditional statement is defined

---
+++ 03_javascript/control_flow_javascript.js
@@ -0,0 +1,51 @@
+/* control flow of the javascript is started we will learn in this chapter deeply the javascript flow how it's work
+*/
+// if
+//sample of if statement
+/*if (condition) {    
+}*/
+/* const isUserLoggedIn = true
+if (isUserLoggedIn) {
+    console.log("congratulation you are logged in");
+}*/
+// operators to compare the value
+// <, >, ,<=, >=, ==, !=(not equal to), ===(strictly check to the value)
+/* if (2 == "2") {
+     console.log("go ahead");
+ }*/
+// output will "go ahead"
+/* if (2 === "2") {
+     console.log("go ahead");    
+} else {
+    console.log("please put the correct value");
+ } */
+// output will be "please put the correct value because it doesn't match with the value why it's happened beacause we have putted different value one is number and another is string"
+/*if (3 != 3) {
+    console.log("congrats you did good");
+}else {
+    console.log("please check the value");    
+} */
+// output will be please check the value because 3 is equal to 3 so we got the else code result
+/* let tem = 51
+if (tem < 40) {
+    console.log("today tempreture is a little bit down");
+} else {
+    console.log("be safe today will be hot climate");
+}*/
+// output will be safe today will be hot climate
+/*const MyAccountBalance = 15000
+if (MyAccountBalance > 17000 ) {
+    console.log(`your current balance will be remain ${ 15000 - 14999}`);
+} else {
+    console.log("you can't go ahead");
+}*/
+/*
+const UserLoggedIn = true
+const debitcard = true
+const userName = "Pritam"
+const loggedInWithGmail = true
+const loggedInWithPhone = false
+if (UserLoggedIn && debitcard && userName !== "" & loggedInWithGmail || loggedInWithPhone) {
+    console.log( `you can sale or buy your product ${userName}`);
+}
+*/
