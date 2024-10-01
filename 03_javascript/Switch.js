b73e4fe20d8b8ceea6326756caec83622b66d7bd
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-10-01 12:54:34 +0000

    concept and example of swith updated

---
+++ 03_javascript/Switch.js
@@ -0,0 +1,25 @@
+/*
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}*/
+const month = 2
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+
+    default:
+        console.log("we can write here a message which will be show on the not match of the any value");
+        break;
+}
