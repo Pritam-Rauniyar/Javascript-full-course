335b93d269ebf8e4bb4d46cba5ee50ccc8d2ce55
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-10-01 13:17:51 +0000

    truthy and operator to compare is updated

--- 03_javascript/truthy.js
+++ 03_javascript/truthy.js
@@ -17,3 +17,17 @@ const emtobj = {}
 if (Object.keys(emtobj).length === 0) {
     console.log("Object is empty");
 }
+// Nullish Coalescing Operator (???): null and undefined
+let val1;
+//  val1  = 5 ?? 10
+//  val1  = null ?? 10
+//  val1 = undefined ?? 30
+// val1 = null ?? 50 ?? 23
+console.log(val1);
+// terniary Operator
+// condition ? ture : false
+
+// const JsBook = 300
+// JsBook <= 250 ? console.log("more than 300") : console.log("less than 300");
+// ;
+
