b17ccbd584cd7af55616ca804ac73688fd718195
Pritam-Rauniyar <152363898+Pritam-Rauniyar@users.noreply.github.com>
2024-10-02 03:55:59 +0000

    some concept of the map and filter are cover in this seriec

---
+++ 05_javascript/01_map_filter_reduce.js
@@ -0,0 +1,26 @@
+// filter
+const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNums = myNum.filter((val) => val > 6 )
+// console.log(newNums);
+ const newNums = []
+ myNum.forEach((val) => {
+    if (val > 5) {
+        newNums.push(val)
+    }
+ })
+//  console.log(newNums);
+const books = [
+    {title: "Book One", genre: "Fiction", publish: 1981},
+    {title: "Book Two", genre: " Non-Fiction", publish: 1992},
+    {title: "Book Three", genre: "History", publish: 1999},
+    {title: "Book Four", genre: "Science", publish: 1989}, 
+    {title: "Book Five", genre: "Non-Fiction", publish: 2009},
+    {title: "Book Six", genre: "Fiction", publish: 1987},
+    {title: "Book Seven", genre: "History", publish: 1986},
+    {title: "Book Eight", genre: "Science", publish: 2011},
+    {title: "Book Nine", genre: "Non-Fiction", publish: 1981},
+]
+const UserBooks = books.filter((bk)=> bk.genre === "History")
+console.log(UserBooks);
+const UserBooks2 = books.filter((bk)=> bk.publish >= 2000 && bk.genre === "Science")
+console.log(UserBooks2);
